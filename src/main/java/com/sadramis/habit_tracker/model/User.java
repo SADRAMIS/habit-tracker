9ff84e4c704251813a7c83221db0fb69851d0800
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2026-05-29 22:44:44 +0300

    Implement User entity with validation and constraints

---
+++ src/main/java/com/sadramis/habit_tracker/model/User.java
@@ -0,0 +1,25 @@
+package com.sadramis.habit_tracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "ROLE_USER";
+}
