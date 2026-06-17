ff191e0a288fea765410f78705f82dc55b9565d7
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2026-06-17 14:30:09 +0300

    Add Kafka producer to ProgressService for goal achieved event

--- src/main/java/com/sadramis/habit_tracker/service/ProgressService.java
+++ src/main/java/com/sadramis/habit_tracker/service/ProgressService.java
@@ -1,22 +1,28 @@
 package com.sadramis.habit_tracker.service;
 
 import com.sadramis.habit_tracker.dto.ProgressRequest;
+import com.sadramis.habit_tracker.event.GoalAchievedEvent;
 import com.sadramis.habit_tracker.exception.GoalNotFoundException;
 import com.sadramis.habit_tracker.model.Goal;
 import com.sadramis.habit_tracker.model.Progress;
 import com.sadramis.habit_tracker.repository.GoalRepository;
 import com.sadramis.habit_tracker.repository.ProgressRepository;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class ProgressService {
 
     private final ProgressRepository progressRepository;
     private final GoalRepository goalRepository;
+    private final KafkaTemplate<String, GoalAchievedEvent> kafkaTemplate;
 
-    public ProgressService(ProgressRepository progressRepository, GoalRepository goalRepository) {
+    public ProgressService(ProgressRepository progressRepository, GoalRepository goalRepository, KafkaTemplate<String, GoalAchievedEvent> kafkaTemplate) {
         this.progressRepository = progressRepository;
         this.goalRepository = goalRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public void addProgress(ProgressRequest request, Long userId) {
@@ -29,5 +35,21 @@ public class ProgressService {
         progress.setDate(request.getDate());
 
         progressRepository.save(progress);
+
+        Double sum = progressRepository.sumValueByGoalId(goal.getId());
+        if (sum == null) {
+            sum = 0.0;
+        }
+
+        double previousSum = sum - request.getProgressValue();
+        if (previousSum < goal.getTargetValue() && sum >= goal.getTargetValue()) {
+            GoalAchievedEvent event = new GoalAchievedEvent(
+                    goal.getId(),
+                    userId,
+                    "Поздравляем! Вы достигли цели \"" + goal.getTitle() + "\"",
+                    Instant.now()
+                    );
+            kafkaTemplate.send("goal-events",goal.getId().toString(),event);
+        }
     }
 }
